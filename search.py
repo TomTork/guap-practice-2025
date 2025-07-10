b83421fad63f53b9cfc3b36c153a511edb90ba69
tomtork <dima26483950@gmail.com>
2025-07-10 18:05:58 +0300

    rc 1.0

--- search.py
+++ search.py
@@ -7,24 +7,84 @@ File = ''
 
 def run():
     global Destination, Search, File
+
+    if not Search.strip():
+        return consts.primary('Search pattern is empty!')
+
+    result = []
     search_text = helper.remove_not_necessarily_symbols(Search.lower())
+
     try:
-        source_text = open(File, 'r').read().lower()
+        with open(File, 'r') as file:
+            source_text = file.read().lower()
     except BaseException as error:
         return consts.primary(error)
+
     translated_text = helper.remove_not_necessarily_symbols(source_text)
     search_numerate = []
     translated_numerate = []
-    pos = 0
     for word in search_text.split():
-        search_numerate.append((word, pos, pos + len(word) - 1))
-        pos += len(word) + 1
+        search_numerate.append(word)
     pos = 0
     for word in translated_text.split():
-        translated_numerate.append((word, pos, pos + len(word) - 1))
-        pos += len(word) + 1
-    print(search_numerate)
-    print(translated_numerate)
+        start_pos = source_text.find(word, pos)
+        translated_numerate.append((word, start_pos))
+        pos = start_pos + len(word)
+
+    for i in range(len(translated_numerate)):
+        ans_start_pos = -1
+        ans_end_pos = -1
+        for index in range((len(search_numerate))):
+            if i + index >= len(translated_numerate):
+                break
+            state = check_run_levenshtein(translated_numerate[i + index][0], search_numerate[index])
+            if -1 < state <= get_degree_of_error(search_numerate[index]):
+                if index == 0:
+                    ans_start_pos = translated_numerate[i + index][1]
+                if index == len(search_numerate) - 1:
+                    ans_end_pos = translated_numerate[i + index][1] + len(translated_numerate[i + index][0])
+            else:
+                break
+        if ans_start_pos != -1 and ans_end_pos != -1:
+            result.append((ans_start_pos, ans_end_pos))
+    return result
 
 
-    return 'result'
+def check_run_levenshtein(s1, s2):
+    if abs(len(s1) - len(s2) > 4):
+        return -1
+    return levenshtein_distance(s1, s2)
+
+
+# Получить степень допустимой ошибки
+def get_degree_of_error(s):
+    if len(s) <= 2:
+        return 0
+    if len(s) <= 4:
+        return 1
+    if len(s) <= 6:
+        return 2
+    if len(s) <= 8:
+        return 3
+    return 4
+
+
+# Расстояние Левенштейна
+def levenshtein_distance(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein_distance(s2, s1)
+    prev_row = list(range(len(s2) + 1))
+    for i, c1 in enumerate(s1, 1):
+        curr_row = [i]
+        for j, c2 in enumerate(s2, 1):
+            if c1 == c2:
+                cost = 0
+            else:
+                cost = 1
+            curr_row.append(min(
+                prev_row[j] + 1,  # Операция удаления
+                curr_row[j - 1] + 1,  # Операция вставки
+                prev_row[j - 1] + cost  # Операция замены
+            ))
+        prev_row = curr_row
+    return prev_row[-1]
